4f94433db74129c42e71388ff43e9c663d1254ad
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-08-28 12:19:17 -0300

    update create folders

---
+++ api/src/controllers/TasksController.js
@@ -0,0 +1,14 @@
+class TasksController {
+  create(request, response) {
+    const { title, description } = request.body;
+    response.json({ title, description });
+  }
+  update(request, response) {
+    const { title, description } = request.body;
+    response.json({ title, description });
+  }
+  delete(request, response) {
+    const { id } = request.params;
+    response.json();
+  }
+}
