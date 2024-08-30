f107afdb789c3396d2ecdb159569df81a09fb4e7
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-08-30 00:28:13 -0300

    update in migrations

--- api/src/database/knex/migrations/20240830025107_createTasks.js
+++ api/src/database/knex/migrations/20240830025107_createTasks.js
@@ -3,6 +3,7 @@ exports.up = (Knex) =>
     table.increments("id");
     table.text("title");
     table.text("description");
+    table.integer("user_id").references("id").inTable("users");
 
     table.timestamp("created_at").default(Knex.fn.now());
     table.timestamp("updated_at").default(Knex.fn.now());
