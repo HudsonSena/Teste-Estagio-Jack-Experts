35fffe970be880247953d405a75c65a26d132d7a
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-09-09 00:52:07 -0300

    Update auth.jsx

--- gerenciador-de-tarefas/src/hooks/auth.jsx
+++ gerenciador-de-tarefas/src/hooks/auth.jsx
@@ -1,8 +1,21 @@
 import { createContext, useContext } from "react";
+import { api } from "../services/api";
 
 const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possivel entrar");
+      }
+    }
+  }
+
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
 }
 
